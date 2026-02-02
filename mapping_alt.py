5aaff73348174ef36d8ac04cbcc9236f0fa09d8d
kian <kian@Mac.ht.home>
2026-02-01 20:54:43 -0600

    implemented way to have car avoid obstacle and continue

--- mapping_alt.py
+++ mapping_alt.py
@@ -23,15 +23,31 @@ def main():
                 time.sleep(0.3)
                 px.backward(MoveDistance)
                 time.sleep(0.5)
-                px.forward(0)  
-                time.sleep(0.3)
-                px.set_dir_servo_angle(sweepangle)
-                sweepangle += 5
-                time.sleep(0.5)
-                if sweepangle > 30:
-                    sweepangle = -30
+                px.forward(0)
+                sweepangle = -30
+                found_path = False
+                while not found_path:
+                    px.set_dir_servo_angle(sweepangle)
+                    time.sleep(0.2)
+                    checkdistance = round(px.ultrasonic.read(), 2)
+                    if checkdistance > SafeDistance:
+                        found_path = True
+                    else:
+                        sweepangle += 10
+                        if sweepangle > 30:
+                            px.backward(MoveDistance)
+                            time.sleep(0.5)
+                            sweepangle = -30
+
+                px.forward(MoveDistance)
+                time.sleep(1.0)
+                px.set_dir_servo_angle(-sweepangle)
+                time.sleep(1.0)
+                px.set_dir_servo_angle(0) #make sure to calibrate if not working
+                time.sleep(0.2)
+            # otherwise, drive straight while sweeping the servo to look for obstacles
             else:
-                px.set_dir_servo_angle(sweepangle)
+                px.set_dir_servo_angle(0)
                 px.forward(MoveDistance)
                 
 
